642caa6611fa1171aa6458617cfc383b26d49d02
Alanna423 <alanna.zhang423@gmail.com>
2024-02-24 17:03:27 -0500

    basic navbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,9 +58,8 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("androidx.compose.ui:ui-text-android:1.6.2")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -68,4 +67,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    val navVersion = "2.7.7"
+    implementation("androidx.navigation:navigation-compose:$navVersion")
 }
---
+++ app/src/main/java/com/example/courseexpert/Navbar.kt
@@ -0,0 +1,26 @@
+package com.example.courseexpert
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun Navbar(navController: NavController, modifier: Modifier) {
+    Surface() {
+        Row(
+            modifier = modifier
+        ) {
+            AppScreens.values().forEach { screen ->
+                Button(
+                    onClick = {navController.navigate(screen.name)}
+                ) {
+                    Text(screen.name)
+                }
+            }
+        }
+    }
+}
