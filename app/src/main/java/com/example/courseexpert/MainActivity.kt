70d6ae61f2585218cba2cba4d0691df165e2d459
Alanna423 <alanna.zhang423@gmail.com>
2024-02-24 15:53:32 -0500

    add composable function to Screens

--- app/src/main/java/com/example/courseexpert/MainActivity.kt
+++ app/src/main/java/com/example/courseexpert/MainActivity.kt
@@ -8,8 +8,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.courseexpert.ui.OnboardingScreen
 import com.example.courseexpert.ui.theme.CourseExpertTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +35,11 @@ fun CourseExpertApp() {
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        Text("Alanna branch.")
+        var showOnboarding by remember { mutableStateOf(true) }
+
+        if (showOnboarding) {
+            OnboardingScreen()
+        }
     }
 }
 
--- app/src/main/java/com/example/courseexpert/ui/OnboardingScreen.kt
+++ app/src/main/java/com/example/courseexpert/ui/OnboardingScreen.kt
@@ -1,2 +1,9 @@
 package com.example.courseexpert.ui
 
+import androidx.compose.runtime.Composable
+
+// this component is created from MainActivity.kt's CourseExpertApp Composable
+@Composable
+fun OnboardingScreen() {
+
+}
