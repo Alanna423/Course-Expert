9ab4bf3b5a19b4b972ac1377db3246100c4ee98b
Alanna423 <alanna.zhang423@gmail.com>
2024-02-24 21:14:02 -0500

    rough search result display of db items

--- app/src/main/java/com/example/courseexpert/ui/SearchScreen.kt
+++ app/src/main/java/com/example/courseexpert/ui/SearchScreen.kt
@@ -1,10 +1,25 @@
 package com.example.courseexpert.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import com.example.courseexpert.data.Review
 import com.google.firebase.firestore.FirebaseFirestore
 
 // this component is created from MainActivity.kt's CourseExpertApp Composable
 @Composable
 fun SearchScreen(reviewDb: FirebaseFirestore) {
+    val reviewList = remember { mutableStateListOf<Review>() }
 
+    reviewDb.collection("reviews").get()
+        .addOnSuccessListener{ list ->
+        for (item in list) {
+            val review = item.toObject(Review::class.java)
+            reviewList.add(review)
+        }
+    }
+
+    for (review in reviewList) {
+        ReviewPreview(review)
+    }
 }
