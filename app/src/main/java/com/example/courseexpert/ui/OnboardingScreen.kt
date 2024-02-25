33466f8ae21677d79c6a212468520c2f7921c61f
Naga Ritvika Yeyuvuri <nryeyuvu@unc.edu>
2024-02-25 02:48:41 -0500

    List of courses to SearchScreen

--- app/src/main/java/com/example/courseexpert/ui/OnboardingScreen.kt
+++ app/src/main/java/com/example/courseexpert/ui/OnboardingScreen.kt
@@ -1,23 +1,15 @@
 package com.example.courseexpert.ui
 
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ProvideTextStyle
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -27,13 +19,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
@@ -41,8 +31,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.courseexpert.R
-import com.example.courseexpert.ui.theme.CourseExpertTheme
-import java.time.format.TextStyle
 
 //@Composable
 //fun CourseExpertApp() {
@@ -160,5 +148,5 @@ fun OnboardingScreen() {
             Text("Create Account", fontSize = 25.sp)
         }
     }
-    }
+}
 //@Composable
