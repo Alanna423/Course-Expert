ef2a4b024cafe5fc0f9700556dbd63369f5bf707
Naga Ritvika Yeyuvuri <nryeyuvu@unc.edu>
2024-02-24 19:44:38 -0500

    Pulling from main

--- app/src/main/java/com/example/courseexpert/MainActivity.kt
+++ app/src/main/java/com/example/courseexpert/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,16 +13,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.example.courseexpert.ui.AddReviewScreen
-import com.example.courseexpert.ui.FAQScreen
 import com.example.courseexpert.ui.OnboardingScreen
-import com.example.courseexpert.ui.ProfileScreen
-import com.example.courseexpert.ui.SearchScreen
 import com.example.courseexpert.ui.theme.CourseExpertTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,53 +29,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CourseExpertApp(appNavController: NavHostController = rememberNavController()) {
+fun CourseExpertApp() {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        var showOnboarding by remember { mutableStateOf(false) }
-
-        val backStackEntry by appNavController.currentBackStackEntryAsState()
-        var currentScreen = AppScreens.valueOf(backStackEntry?.destination?.route ?: AppScreens.Search.name)
+        var showOnboarding by remember { mutableStateOf(true) }
 
         if (showOnboarding) {
             OnboardingScreen()
-        } else {
-            Scaffold(
-                bottomBar = {
-                    Navbar(appNavController, modifier = Modifier)
-                }
-            ) { innerPadding ->
-                NavHost(
-                    navController = appNavController,
-                    startDestination = currentScreen.name,
-                    modifier = Modifier.padding(innerPadding)
-                ) {
-                    composable(route = AppScreens.AddReview.name) {
-                        AddReviewScreen()
-                    }
-                    composable(route = AppScreens.FAQ.name) {
-                        FAQScreen()
-                    }
-                    composable(route = AppScreens.Profile.name) {
-                        ProfileScreen()
-                    }
-                    composable(route = AppScreens.Search.name) {
-                        SearchScreen()
-                    }
-                }
-            }
         }
     }
 }
 
-enum class AppScreens() {
-    AddReview,
-    FAQ,
-    Profile,
-    Search
-}
 
 @Preview(showBackground = true)
 @Composable
