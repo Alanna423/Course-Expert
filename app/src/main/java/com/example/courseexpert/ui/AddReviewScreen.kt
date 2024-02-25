4991eb2da6b0769a5eac89865b871a1ca16800cb
Naga Ritvika Yeyuvuri <nryeyuvu@unc.edu>
2024-02-25 00:02:54 -0500

    Adding functionality to review screen

--- app/src/main/java/com/example/courseexpert/ui/AddReviewScreen.kt
+++ app/src/main/java/com/example/courseexpert/ui/AddReviewScreen.kt
@@ -1,17 +1,25 @@
 package com.example.courseexpert.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Switch
@@ -23,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.courseexpert.ui.theme.CourseExpertTheme
@@ -44,6 +53,7 @@ fun AddReviewScreen() {
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
+            .verticalScroll(rememberScrollState())
     ) {
         OutlinedTextField(
             value = courseDepartment,
@@ -58,6 +68,15 @@ fun AddReviewScreen() {
             value = courseNumber,
             onValueChange = { courseNumber = it },
             label = { Text("Course Number") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
+
+        OutlinedTextField(
+            value = professor,
+            onValueChange = { professor = it },
+            label = { Text("Professor Name") },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp)
@@ -124,6 +143,22 @@ fun AddReviewScreen() {
             )
         }
 
+        Text("Course Review")
+        BasicTextField(
+            value = courseReview,
+            onValueChange = { courseReview = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Text
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .background(MaterialTheme.colorScheme.background)
+                .border(1.dp, MaterialTheme.colorScheme.primary)
+                .padding(16.dp)
+                .heightIn(min = 100.dp)
+        )
+
         Button(
             onClick = {
                 // add submission logic
@@ -142,6 +177,11 @@ fun AddReviewScreen() {
 @Composable
 fun AddReviewScreenPreview() {
     CourseExpertTheme {
-        AddReviewScreen()
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+        ) {
+            AddReviewScreen()
+        }
     }
 }
